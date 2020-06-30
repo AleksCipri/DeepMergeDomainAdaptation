visualize gradients, fixed filepaths

galaxy_merge_edits/galaxy_train.py
@@ -21,10 +21,9 @@ from torch.utils.data import Dataset, TensorDataset, DataLoader
 from torch.autograd import Variable
 from galaxy_utils import EarlyStopping, image_classification_test, distance_classification_test, domain_cls_accuracy
 from import_and_normalize import array_to_tensor, update
+from plot_gradients import plot_grad_flow
 
 optim_dict = {"SGD": optim.SGD, "Adam": optim.Adam}
-
-#import the preprocessed tensors
 
 def train(config):
     ## set up summary writer
@@ -33,8 +32,7 @@ def train(config):
     class_criterion = nn.CrossEntropyLoss()
 
     transfer_criterion = config["loss"]["name"]
-    center_criterion = config["loss"]["discriminant_loss"](num_classes=class_num, 
-                                       feat_dim=config["network"]["params"]["bottleneck_dim"])
+    center_criterion = config["loss"]["discriminant_loss"](num_classes=class_num, feat_dim=config["network"]["params"]["bottleneck_dim"])
     loss_params = config["loss"]
 
     ## prepare data
@@ -124,13 +122,14 @@ def train(config):
             parameter_list = [{"params":base_network.feature_layers.parameters(), "lr_mult":1, 'decay_mult':2}, \
                             {"params":base_network.fc.parameters(), "lr_mult":10, 'decay_mult':2}]
     else:
-        parameter_list = [{"params":base_network.parameters(), "lr_mult":1, 'decay_mult':2}]
+        parameter_list = [{"params":base_network.parameters(), "lr_mult":10, 'decay_mult':2}]
 
     ## add additional network for some methods
     class_weight = torch.from_numpy(np.array([1.0] * class_num))
     if use_gpu:
         class_weight = class_weight.cuda()
-    parameter_list.append({"params":center_criterion.parameters(), "lr_mult": 10, 'decay_mult':1}) ####    WHYYYY?!?!?!  ######
+
+    parameter_list.append({"params":center_criterion.parameters(), "lr_mult": 10, 'decay_mult':1})
  
     ## set optimizer
     optimizer_config = config["optimizer"]
@@ -141,7 +140,6 @@ def train(config):
         param_lr.append(param_group["lr"])
     schedule_param = optimizer_config["lr_param"]
     lr_scheduler = lr_schedule.schedule_dict[optimizer_config["lr_type"]]
-
 
     ## train   
     len_train_source = len(dset_loaders["source"]) - 1
@@ -197,9 +195,8 @@ def train(config):
 
         if (i+1) % config["snapshot_interval"] == 0:
             torch.save(snapshot_obj, 
-                        osp.join(config["output_path"], "epoch_{:05d}_model.pth.tar".format(i/len(dset_loaders["source"]))))
+                        osp.join(config["output_path"], "epoch_{}_model.pth.tar".format(i/len(dset_loaders["source"]))))
                     
-
         ## train one iter
         base_network.train(True)
 
@@ -244,8 +241,8 @@ def train(config):
         # source domain classification task loss
         classifier_loss = class_criterion(source_logits, labels_source.long())
         # fisher loss on labeled source domain
-        fisher_loss, fisher_intra_loss, fisher_inter_loss, center_grad = center_criterion(features.narrow(0, 0, int(inputs.size(0)/2)), labels_source, inter_class=loss_params["inter_type"], 
-                                                                               intra_loss_weight=loss_params["intra_loss_coef"], inter_loss_weight=loss_params["inter_loss_coef"])
+        fisher_loss, fisher_intra_loss, fisher_inter_loss, center_grad = center_criterion(features.narrow(0, 0, int(inputs.size(0)/2)), labels_source, inter_class=loss_params["inter_type"], intra_loss_weight=loss_params["intra_loss_coef"], inter_loss_weight=loss_params["inter_loss_coef"])
+                                                                               
         # entropy minimization loss
         em_loss = loss.EntropyLoss(nn.Softmax(dim=1)(logits))
 
@@ -254,10 +251,6 @@ def train(config):
              + loss_params["em_loss_coef"] * em_loss \
              + classifier_loss
         
-        # final loss
-        #if config["domain_adapt"] == 'False':
-        #    classifier_loss.backward() #we need to fix this
-        #else:
         total_loss.backward()
 
         if center_grad is not None:
@@ -269,6 +262,13 @@ def train(config):
         optimizer.step()
 
         if i % config["log_iter"] == 0:
+
+            if config['grad_vis'] != 'no':
+                if not osp.exists(osp.join(config["output_path"], "gradients")):
+                    os.makedirs(osp.join(config["output_path"], "gradients"))
+
+                plot_grad_flow(osp.join(config["output_path"], "gradients"), i/len(dset_loaders["source"]), base_network.named_parameters())
+
             config['out_file'].write('epoch {}: train total loss={:0.4f}, train transfer loss={:0.4f}, train classifier loss={:0.4f}, '
                 'train entropy min loss={:0.4f}, '
                 'train fisher loss={:0.4f}, train intra-group fisher loss={:0.4f}, train inter-group fisher loss={:0.4f}\n'.format(
@@ -327,8 +327,8 @@ def train(config):
                     # source domain classification task loss
                     classifier_loss = class_criterion(source_logits, labels_source.long())
                     # fisher loss on labeled source domain
-                    fisher_loss, fisher_intra_loss, fisher_inter_loss, center_grad = center_criterion(features.narrow(0, 0, int(inputs.size(0)/2)), labels_source, inter_class=loss_params["inter_type"], 
-                                                                                           intra_loss_weight=loss_params["intra_loss_coef"], inter_loss_weight=loss_params["inter_loss_coef"])
+                    fisher_loss, fisher_intra_loss, fisher_inter_loss, center_grad = center_criterion(features.narrow(0, 0, int(inputs.size(0)/2)), labels_source, inter_class=loss_params["inter_type"], intra_loss_weight=loss_params["intra_loss_coef"], inter_loss_weight=loss_params["inter_loss_coef"])
+                                                                                           
                     # entropy minimization loss
                     em_loss = loss.EntropyLoss(nn.Softmax(dim=1)(logits))
                     
@@ -340,7 +340,7 @@ def train(config):
                     #total_loss.backward() no backprop on the eval mode
 
                 if j % len(dset_loaders["source_valid"]) == 0:
-                    config['out_file'].write('epoch {} valid transfer loss={:0.4f}, valid classifier loss={:0.4f}, '
+                    config['out_file'].write('epoch {}, valid transfer loss={:0.4f}, valid classifier loss={:0.4f}, '
                         'valid entropy min loss={:0.4f}, '
                         'valid fisher loss={:0.4f}, valid intra-group fisher loss={:0.4f}, valid inter-group fisher loss={:0.4f}\n'.format(
                         i/len(dset_loaders["source"]), transfer_loss.data.cpu().float().item(), classifier_loss.data.cpu().float().item(), 
@@ -373,14 +373,23 @@ if __name__ == "__main__":
                         choices=["tr", "td"], 
                         help="type of Fisher loss.")
     parser.add_argument('--inter_type', type=str, default="global", choices=["sample", "global"], help="type of inter_class loss.")
-    #parser.add_argument('--source_size', type=float, default=1.0, help="source domain sampling size")
-    #parser.add_argument('--target_size', type=float, default=1.0, help="target domain sampling size")
     parser.add_argument('--net', type=str, default='ResNet50', help="Options: ResNet18,34,50,101,152; AlexNet")
     parser.add_argument('--dset', type=str, default='galaxy', help="The dataset or source dataset used")
-    parser.add_argument('--dset_path', type=str, default='/arrays', help="The source dataset path")
+    # parser.add_argument('--dset_path', type=str, default='/arrays', help="The source dataset path")
     parser.add_argument('--output_dir', type=str, default='san', help="output directory of our model (in ../snapshot directory)")
     parser.add_argument('--optim_choice', type=str, default='SGD', help='Adam or SGD')
     parser.add_argument('--epochs', type=int, default=200, help='How many epochs do you want to train?')
+    parser.add_argument('--grad_vis', type=str, default='no', help='Do you want to visualize your gradients?')
+    parser.add_argument('--dset_path', type=str, default=None, help="The dataset directory path")
+    parser.add_argument('--source_x_file', type=str, default='SB_version_00_numpy_3_filters_pristine_SB00_augmented_3FILT.npy',
+                         help="Source domain x-values filename")
+    parser.add_argument('--source_y_file', type=str, default='SB_version_00_numpy_3_filters_pristine_SB00_augmented_y_3FILT.npy',
+                         help="Source domain y-values filename")
+    parser.add_argument('--target_x_file', type=str, default='SB_version_00_numpy_3_filters_noisy_SB25_augmented_3FILT.npy',
+                         help="Target domain x-values filename")
+    parser.add_argument('--target_y_file', type=str, default='SB_version_00_numpy_3_filters_noisy_SB25_augmented_y_3FILT.npy',
+                         help="Target domain y-values filename")
+    
     
     args = parser.parse_args()
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
@@ -388,13 +397,10 @@ if __name__ == "__main__":
     # train config
     config = {}
     config["epochs"] = args.epochs
-    config["high"] = 1.0 #should this maybe be .5? the other data ranged from .25 to .5
     config["output_for_test"] = True
     config["output_path"] = args.output_dir
-    config["log_iter"] = 25
-    config["early_stop_patience"] = 100
     config["optim_choice"] = args.optim_choice
-
+    config["grad_vis"] = args.grad_vis
 
     if not osp.exists(config["output_path"]):
         os.makedirs(config["output_path"])
@@ -422,11 +428,11 @@ if __name__ == "__main__":
         config["network"] = {"name":network.ResNetFc, \
             "params":{"resnet_name":args.net, "use_bottleneck":True, "bottleneck_dim":256, "new_cls":True} }
 
-
+    #set optimizer
     if config["optim_choice"] == 'Adam':
-        config["optimizer"] = {"type":"Adam", "optim_params":{"lr":0.001, "betas":(0.9,0.999), "weight_decay":0.01, \
+        config["optimizer"] = {"type":"Adam", "optim_params":{"lr":0.0005, "betas":(0.7,0.8), "weight_decay":0.01, \
                                 "amsgrad":False, "eps":1e-8}, \
-                        "lr_type":"inv", "lr_param":{"init_lr":0.001, "gamma":0.001, "power":0.75} }               
+                        "lr_type":"inv", "lr_param":{"init_lr":0.0005, "gamma":0.001, "power":0.75} }               
     else:
         config["optimizer"] = {"type":"SGD", "optim_params":{"lr":0.001, "momentum":0.9, \
                                "weight_decay":0.0005, "nesterov":True}, "lr_type":"inv", \
@@ -445,20 +451,17 @@ if __name__ == "__main__":
 
     if args.lr is not None:
         config["optimizer"]["optim_params"]["lr"] = args.lr
-    if args.lr is None:
-        config["optimizer"]["optim_params"]["lr"] = 0.0001
-    else:
-         raise ValueError('{} cannot be found. ')
+        config["optimizer"]["lr_param"]["init_lr"] = args.lr
         
     config["dataset"] = args.dset
     config["path"] = args.dset_path
 
-    if config["dataset"] == 'galaxy': 
-        pristine_x = array_to_tensor(osp.join(os.getcwd(), config['path'], 'SB_version_00_numpy_3_filters_pristine_SB00_augmented_3FILT.npy'))
-        pristine_y = array_to_tensor(osp.join(os.getcwd(), config['path'], 'SB_version_00_numpy_3_filters_pristine_SB00_augmented_y_3FILT.npy'))
+    if config["dataset"] == 'galaxy':
+        pristine_x = array_to_tensor(osp.join(config['path'], args.source_x_file))
+        pristine_y = array_to_tensor(osp.join(config['path'], args.source_y_file))
 
-        noisy_x = array_to_tensor(osp.join(os.getcwd(), config['path'], 'SB_version_00_numpy_3_filters_noisy_SB25_augmented_3FILT.npy'))
-        noisy_y = array_to_tensor(osp.join(os.getcwd(), config['path'], 'SB_version_00_numpy_3_filters_noisy_SB25_augmented_y_3FILT.npy'))
+        noisy_x = array_to_tensor(osp.join(config['path'], args.target_x_file))
+        noisy_y = array_to_tensor(osp.join(config['path'], args.target_y_file))
 
         update(pristine_x, noisy_x)
 