fae9466084f59805d8fbc944ac80e307bdbd9d70
Diana Kafkes x 40452N <dkafkes@fermi.win.fnal.gov>
2020-07-24 17:03:14 -0500

    new and improved early stopping and able to toggle learning type now

--- galaxy_merge_edits/lr_schedule.py
+++ galaxy_merge_edits/lr_schedule.py
@@ -2,11 +2,11 @@ import numpy as np
 
 def inv_lr_scheduler(param_lr, optimizer, iter_num, epoch_length, extra_lr, extra_cycle, gamma, power, init_lr=0.001, weight_decay=5e-4):
 	"""Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
-	lr = init_lr * (1 + gamma * iter_num) ** (-power)
+	lr = init_lr #* (1 + gamma * iter_num) ** (-power)
 	i=0
 	for param_group in optimizer.param_groups:
-		param_group['lr'] = lr * param_group['lr_mult']
-		param_group['weight_decay'] = weight_decay * param_group['decay_mult']
+		param_group['lr'] = lr #* param_group['lr_mult']
+		#param_group['weight_decay'] = 1 * param_group['decay_mult']
 		i+=1
 
 	return optimizer
@@ -69,7 +69,7 @@ def linear_to_find_baseline(param_lr, optimizer, iter_num, epoch_length, lr, ext
 	# max_lr = 1000*lr
 	num_epochs = 5
 	# lr_spectrum = np.arange(min_lr, num_epochs, max_lr)
-	lr_spectrum = np.linspace(1e-2, 1, 85*num_epochs+1)
+	lr_spectrum = np.linspace(1e-6, 1e-1, 85*num_epochs+1)
 	num_steps = epoch_length
 
 	#on_step = iter_num//epoch_length
