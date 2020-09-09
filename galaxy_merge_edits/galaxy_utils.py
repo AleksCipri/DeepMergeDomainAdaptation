able to save model predictions, fixed softmax in image classification test

galaxy_merge_edits/galaxy_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import os.path
+import sys
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 from collections import deque
@@ -69,7 +70,21 @@ def domain_cls_accuracy(d_out):
     source_acc = float(d0_pred[:batch_size].cpu().float().eq(d0_target[:batch_size]).sum()) / float(batch_size)
     target_acc = float(d0_pred[batch_size:].cpu().float().eq(d0_target[batch_size:]).sum()) / float(batch_size)
 
-    #d0_loss = 
+    # print("adnet output")
+    # print(d_out)
+    # print("batch size")
+    # print(batch_size)
+    # print("target")
+    # print(d0_target)
+    # print("prediction")
+    # print(d0_pred)
+    # print("all accuracy")
+    # print(d0_acc)
+    # print("source accuracy")
+    # print(source_acc)
+    # print("target accuracy")
+    # print(target_acc)
+    # sys.exit()
     
     return d0_acc, source_acc, target_acc
 
@@ -142,11 +157,12 @@ def distance_classification_test(loader, dictionary_val, model, centroids, gpu=T
 
         output = pd.DataFrame()
 
-        # print("all output")
-        # print(torch.max(all_output, 1)[1].cpu().detach().numpy())
-        # print()
-        # print("all label")
-        # print(all_label.cpu().detach().numpy())
+        print("all output")
+        print(all_output.cpu().detach().numpy())
+        print(torch.max(all_output, 1)[1].cpu().detach().numpy())
+        print()
+        print("all label")
+        print(all_label.cpu().detach().numpy())
 
         output['model output'] = pd.Series(torch.max(all_output, 1)[1].cpu().detach().numpy())
         output['labels'] = pd.Series(all_label.cpu().detach().numpy())
@@ -190,15 +206,20 @@ def image_classification_test(loader, dictionary_val, model, gpu=True, verbose =
                 inputs = inputs.cuda()
                 labels = labels.cuda()
             _, outputs = model(inputs)
+            softmax_outputs = nn.Softmax(dim=1)(1.0 * outputs)
+
             if start_test:
+                all_softmax_output = softmax_outputs.data.float()
                 all_output = outputs.data.float()
                 all_label = labels.data.float()
                 start_test = False
             else:
+                all_softmax_output = torch.cat((all_softmax_output, softmax_outputs.data.float()), 0)
                 all_output = torch.cat((all_output, outputs.data.float()), 0)
                 all_label = torch.cat((all_label, labels.data.float()), 0)
 
-    _, predict = torch.max(all_output, 1)
+    #_, predict = torch.max(all_output, 1)
+    _, predict = torch.max(all_softmax_output, 1)
     accuracy = torch.sum(torch.squeeze(predict).float() == all_label).float() / float(all_label.size()[0])
     conf_matrix = confusion_matrix(all_label.cpu().numpy(), predict.cpu().numpy())
 
@@ -206,7 +227,10 @@ def image_classification_test(loader, dictionary_val, model, gpu=True, verbose =
 
         output = pd.DataFrame()
 
+        df = pd.DataFrame(all_softmax_output.cpu().detach().numpy(), columns=['non-merger', 'merger'])
+
         # print("all output")
+        # print(all_output.cpu().detach().numpy())
         # print(torch.max(all_output, 1)[1].cpu().detach().numpy())
         # print()
         # print("all label")
@@ -216,6 +240,7 @@ def image_classification_test(loader, dictionary_val, model, gpu=True, verbose =
         output['labels'] = pd.Series(all_label.cpu().detach().numpy())
 
         output.to_csv(str(save_where)+"/model_results.csv")
+        df.to_csv(str(save_where)+"/model_predictions.csv")
 
     return accuracy, conf_matrix
 

galaxy_merge_edits/train_ada_updated.py
@@ -214,19 +214,24 @@ def train(config):
                             'train accuracy' : train_acc,                            
                             }
 
+            if config["loss"]["loss_name"] != "laplacian" and config["loss"]["ly_type"] == "euclidean":
+                snapshot_obj['center_criterion'] = center_criterion.state_dict()
+
             if (i+1) % config["snapshot_interval"] == 0:
                 torch.save(snapshot_obj, 
                        osp.join(config["output_path"], "epoch_{}_model.pth.tar".format(i/len(dset_loaders["source"]))))
 
-            if config["loss"]["loss_name"] != "laplacian" and config["loss"]["ly_type"] == "euclidean":
-                snapshot_obj['center_criterion'] = center_criterion.state_dict()
-
             if temp_acc > best_acc:
                 best_acc = temp_acc
-
-                # save best model
                 torch.save(snapshot_obj, 
-                           osp.join(config["output_path"], "best_model.pth.tar"))
+                               osp.join(config["output_path"], "best_model.pth.tar"))
+
+            # if i/config["test_interval"] >= 1:                                      
+            #     if (np.abs(source_acc_ad -.5) < .5) and (np.abs(target_acc_ad -.5) < .5) and (temp_acc > best_acc):
+            #         print("inside to save")
+            #         # save best model
+            #         torch.save(snapshot_obj, 
+            #                    osp.join(config["output_path"], "best_model.pth.tar"))
 
             log_str = "epoch: {}, {} validation accuracy: {:.5f}, {} training accuracy: {:.5f}\n".format(i/len(dset_loaders["source"]), config['loss']['ly_type'], temp_acc, config['loss']['ly_type'], train_acc)
             config["out_file"].write(log_str)
@@ -588,7 +593,7 @@ if __name__ == "__main__":
     parser.add_argument('--cycle_length', type=int, default = 2, help = 'If using one-cycle learning, how many epochs should one learning rate cycle be?')
     parser.add_argument('--early_stop_patience', type=int, default = 10, help = 'Number of epochs for early stopping.')
     parser.add_argument('--weight_decay', type=float, default = 5e-4, help= 'How much do you want to penalize large weights?')
-    parser.add_argument('--ad_net_mult_lr', type=float, default = .1, help= 'Multiply base net lr by this to get ad net lr.')
+    #parser.add_argument('--ad_net_mult_lr', type=float, default = None, help= 'Multiply base net lr by this to get ad net lr.')
     # parser.add_argument('--beta_1', type=float, default=None, help= 'Set first beta in Adam.')
     # parser.add_argument('--beta_1', type=float, default=None, help= 'Set second beta in Adam.')
     parser.add_argument('--blobs', type=str, default=None, help='Plot blob figures.')
@@ -609,7 +614,6 @@ if __name__ == "__main__":
     config["cycle_length"] = args.cycle_length
     config["early_stop_patience"] = args.early_stop_patience
     config["weight_decay"] = args.weight_decay
-    config["ad_net_mult_lr"] = args.ad_net_mult_lr
     config["blobs"] = args.blobs
 
     if not osp.exists(config["output_path"]):
@@ -655,6 +659,11 @@ if __name__ == "__main__":
 
     if args.one_cycle is not None:
         config["optimizer"]["lr_type"] = "one-cycle"
+
+    if config["fisher_or_no"] == 'Fisher':
+        config["ad_net_mult_lr"] = .1
+    else:
+        config["ad_net_mult_lr"] = 1
 
     if args.lr_scan == "yes":
         config["optimizer"]["lr_type"] = "linear"