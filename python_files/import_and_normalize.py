be999b90b383225b3908b4b20f8563f4b52ea671
Diana Kafkes <dianakafkes@gmail.com>
2021-02-17 15:10:52 -0600

    Update import_and_normalize.py

--- python_files/import_and_normalize.py
+++ python_files/import_and_normalize.py
@@ -1,10 +1,4 @@
-
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  4 13:30:47 2020
-@author: dk
-"""
-
+#Importing needed packages
 import torch
 import numpy as np
 from torch.utils.data import Dataset, TensorDataset, DataLoader
@@ -14,7 +8,6 @@ import matplotlib.pyplot as plt
 def array_to_tensor(name):
   data_list = list(np.load(str(name)))
   return(torch.Tensor(data_list))
-
 
 def normalization(t):
 
@@ -28,7 +21,6 @@ def normalization(t):
 
   return np.array([[mean1, mean2, mean3], [std1, std2, std3]])
 
-
 def update(t1, t2):
   
   # find pixel means and stds for a given dataset
@@ -39,8 +31,7 @@ def update(t1, t2):
   pr_trf = transform.Normalize(mean = pristine[0], std = pristine[1], inplace=True)
   no_trf = transform.Normalize(mean = noisy[0], std = noisy[1], inplace=True)
 
-
-  ## in case of transfer learning we have to normalize new images to old means and stds 
+  ## for transfer learning we have to normalize new images to old means and stds 
   ## we just directly input these numbers here
   
   # pristine = [[0.0375126, 0.03326255, 0.06786563],[1.30893517, 1.02839041, 1.12307501]]
